c5eaac207770babd6d60e3d108b741fd02eaf5af
Nuttyboy31 <nuttyboy31nicolas.huard@gmail.Com>
2018-01-17 12:16:26 +0100

    resolutions adb
    obtenirSolutionsPossibles

--- resolutions.adb
+++ resolutions.adb
@@ -14,52 +14,62 @@ package body resolutions is
       if not caseVide(c) then
          raise CASE_NON_VIDE
 
-      return estChiffreValable (g => g, v => v, c => c);
-   end estChiffreValable;
+         return estChiffreValable (g => g, v => v, c => c);
+      end estChiffreValable;
 
-   -------------------------------
-   -- obtenirSolutionsPossibles --
-   -------------------------------
+      -------------------------------
+      -- obtenirSolutionsPossibles --
+      -------------------------------
 
-   function obtenirSolutionsPossibles
-     (g : in Type_Grille;
-      c : in Type_Coordonnee)
-      return Type_Ensemble
-   is
-   begin
-      --  Generated stub: replace with real body!
-      pragma Compile_Time_Warning (Standard.True, "obtenirSolutionsPossibles unimplemented");
-      raise Program_Error with "Unimplemented function obtenirSolutionsPossibles";
-      return obtenirSolutionsPossibles (g => g, c => c);
-   end obtenirSolutionsPossibles;
+      function obtenirSolutionsPossibles
+        (g : in Type_Grille;
+         c : in Type_Coordonnee)
+         return Type_Ensemble
+      is
+         i : Integer; -- Indice tableau
+         e : Type_Ensemble; -- Ensemble des valeurs possibles
+         possible : Boolean; -- Solution possible
 
-   ------------------------------------------
-   -- rechercherSolutionUniqueDansEnsemble --
-   ------------------------------------------
+      begin
+         -- Exceptions
+         if not caseVide(g,c) then
+            raise CASE_NON_VIDE;
+         end if;
+         -- Body
+         for i in 1..9 loop
+            possible := estChiffreValable(g,i,c);
+            if possible then
+               e(i) := TRUE;
+            end if;
+         end loop;
+         return e;
+      end obtenirSolutionsPossibles;
 
-   function rechercherSolutionUniqueDansEnsemble
-     (resultats : in Type_Ensemble)
-      return Integer
-   is
-   begin
-      --  Generated stub: replace with real body!
-      pragma Compile_Time_Warning (Standard.True, "rechercherSolutionUniqueDansEnsemble unimplemented");
-      raise Program_Error with "Unimplemented function rechercherSolutionUniqueDansEnsemble";
-      return rechercherSolutionUniqueDansEnsemble (resultats => resultats);
-   end rechercherSolutionUniqueDansEnsemble;
+      ------------------------------------------
+      -- rechercherSolutionUniqueDansEnsemble --
+      ------------------------------------------
 
-   --------------------
-   -- resoudreSudoku --
-   --------------------
+      function rechercherSolutionUniqueDansEnsemble
+        (resultats : in Type_Ensemble)
+         return Integer
+      is
+      begin
+         --  Generated stub: replace with real body!
+         pragma Compile_Time_Warning (Standard.True, "rechercherSolutionUniqueDansEnsemble unimplemented");
+         raise Program_Error with "Unimplemented function rechercherSolutionUniqueDansEnsemble";
+         return rechercherSolutionUniqueDansEnsemble (resultats => resultats);
+      end rechercherSolutionUniqueDansEnsemble;
 
-   procedure resoudreSudoku
-     (g      : in out Type_Grille;
-      trouve :    out Boolean)
-   is
-   begin
-      --  Generated stub: replace with real body!
-      pragma Compile_Time_Warning (Standard.True, "resoudreSudoku unimplemented");
-      raise Program_Error with "Unimplemented procedure resoudreSudoku";
-   end resoudreSudoku;
+      --------------------
+      -- resoudreSudoku --
+      --------------------
 
-end resolutions;
+      procedure resoudreSudoku
+        (g      : in out Type_Grille;
+         trouve :    out Boolean)
+      is
+      begin
+
+      end resoudreSudoku;
+
+   end resolutions;
